b298da5a87e7449024fcce84717e4a3ae5802316
Ali KOZAN <ankozan@mail.fhsu.edu>
2021-09-18 11:15:34 -0700

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+let myNumber = 5;
+let myOtherNumber = 5;
+let myTotal = myNumber * myOtherNumber;
+let myName = "";
+
+
+let myCombo = (myNumber + myOtherNumber).toString();
+let myOtherCombo = myNumber + myOtherNumber;
+
+let myRemainder = myNumber % 5;
+
+let i  = 2;
+let int = 4;
+let myBoolean = false;
+
+
+
+
+
